b6c9ef66f602942e04c14a1497ccb0a4a08d439a
lStiven <stiven.pinzon@correounivalle.edu.co>
2017-10-25 18:19:30 -0500

    First Commit

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Stiven®
+ * @Date 25/10/2017
+ * @hour 6:17:56 p. m.
+ * @projectName HelloWorld
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
